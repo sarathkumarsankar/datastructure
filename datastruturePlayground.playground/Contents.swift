fcd22bdd418b20ec43a407d5cfab72551aa2264c
Sarathkumar <27sarathkumar.s@gmail.com>
2017-12-22 16:46:24 +0530

    s

--- datastruturePlayground.playground/Contents.swift
+++ datastruturePlayground.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import UIKit
 
-/********* Find most common element in the array ************/
+/********* Find most common element in the array start ************/
 class fruitClass
 {
     init() {
@@ -36,5 +36,35 @@ var fruitsArray = ["s","a","r","a","t","h","k","u","m","a","r"]
 //var fruitsArray = [12,2,3,2,1,6,6,6]
 print("reapetedelement: \(fruitInstance.mostCommonElement(fruitsArray: fruitsArray) )")
 
+/********* Find most common element in the array end************/
 
-
+class fruitClass1
+{
+    init() {
+    }
+    func mostCommonElement(fruitsArray:Array<Any>) -> Any {
+        let fruitDic = NSMutableDictionary()
+        for fruit in fruitsArray {
+            if fruitDic[fruit] != nil {
+                fruitDic[fruit] = fruitDic[fruit] as! Int + 1
+                continue
+            }
+            fruitDic[fruit] = 1
+        }
+        print(fruitDic)
+        var repeatedFruits = fruitsArray[0]
+        var repeatedCount = fruitDic[fruitsArray[0]] as! Int
+        for i in 1..<fruitsArray.count {
+            if repeatedCount <  fruitDic[fruitsArray[i]] as! Int{
+                repeatedFruits = fruitsArray[i]
+                repeatedCount = fruitDic[fruitsArray[i]] as! Int
+                
+            }
+        }
+        return repeatedFruits
+    }
+}
+var fruitInstance = fruitClass1()
+var fruitsArray = [1,"a","r","a","t","h","k","u","m","a","r"]
+//var fruitsArray = [12,2,3,2,1,6,6,6]
+print("reapetedelement: \(fruitInstance.mostCommonElement(fruitsArray: fruitsArray) )")
